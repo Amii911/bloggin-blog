1da081cd9c1a80c3c09f0e5d3facda1a62dc52bf
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-26 07:25:25 -0400

    updates

--- bloggin-blog/src/App.js
+++ bloggin-blog/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   
   return (
     <div className="App">
+      <Navbar/>
       <div className= "content"></div>
       <h1>App Component</h1>
     </div>
