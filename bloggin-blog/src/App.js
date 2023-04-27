7620ecaa09db8749478023bea32633f1365cdfe9
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-27 10:34:23 -0400

    updates

--- bloggin-blog/src/App.js
+++ bloggin-blog/src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './Components/Navbar';
-import Home from './Components/Home'
+import Home from './Components/Home';
+import Create from './Components/Create'
+import{ BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return(
+    <Router>
   <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
