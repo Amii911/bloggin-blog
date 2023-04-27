74bfc3c975f671117520d3bb46e09ec1bb3879f0
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-26 21:38:46 -0400

    update

--- bloggin-blog/src/Components/Navbar.js
+++ bloggin-blog/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
     return (
         <nav className="navbar">
-          <h1>The Dojo Blog</h1>
+          <h1>The Bloggin Blog</h1>
           <div className="links">
             <a href="/">Home</a>
             <a href="/create">New Blog</a>
