6c6e25bd5085e741498902d0d106e805df9efe2c
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-27 13:02:03 -0400

    update

--- bloggin-blog/src/Components/Create.js
+++ bloggin-blog/src/Components/Create.js
@@ -3,30 +3,55 @@ import { useState } from 'react';
 
 const Create = () => {
     const [ title, setTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ author, setAuthor ] = useState('blogger');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author};
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"}
+        })
+    }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
         type="text"
         required
         value={title}
+        onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
         required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author</label>
-        <select>
+        <select 
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}>
+            <option value=" "></option> 
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
         </select>
-        <button>Add button</button>
+        <button>ADD BLOG</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
       </form>
     </div>
   );
 }
  
 export default Create;
+
+
+
+// how can I add author tag to allow folks to create an author name
