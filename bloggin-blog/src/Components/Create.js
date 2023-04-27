74884f3d69d8d16ecd354a8040d33e4a3da12d49
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-27 13:13:35 -0400

    update

--- bloggin-blog/src/Components/Create.js
+++ bloggin-blog/src/Components/Create.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    const [ title, setTitle ] = useState('');
-    const [ body, setBody ] = useState('');
-    const [ author, setAuthor ] = useState('blogger');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const history = useHistory();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = { title, body, author};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
-        fetch('http://localhost:8000/blogs', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(blog)
-        }) .then(() => {
-            console.log('new blog added');
-        })
-    }
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
 
   return (
     <div className="create">
@@ -25,36 +28,32 @@ const Create = () => {
       <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
-        type="text"
-        required
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
-        required
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        <label>Blog author</label>
-        <select 
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}>
-            <option value=" "></option> 
-            <option value="mario">mario</option>
-            <option value="yoshi">yoshi</option>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
         </select>
-        <button>ADD BLOG</button>
-        <p>{ title }</p>
-        <p>{ body }</p>
-        <p>{ author }</p>
+        <button>Add Blog</button>
       </form>
     </div>
   );
 }
  
 export default Create;
-
 
 
 // how can I add author tag to allow folks to create an author name
