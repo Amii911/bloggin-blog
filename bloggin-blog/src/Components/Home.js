af33ba2e882622657414fdeb79f34b77a25d2d93
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-04-28 14:43:54 -0400

    update home

--- bloggin-blog/src/Components/Home.js
+++ bloggin-blog/src/Components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home">
       <h2>WELCOME TO BLOGGIN BLOG</h2>
       <br/>
-      <p>Bloggin Blog is a space for users to share their story, speak their mind and even hang onto a memeory. Users are able to build, manage, and delete blogs easily with Bloggin Blog. Join millions today and create a blog with Bloggin Blog. </p>
+      <p>Bloggin Blog is a space for users to share their story, speak their mind and even hang onto a memory. Users are able to build, manage, and delete blogs easily with Bloggin Blog. Join millions today and create a blog with Bloggin Blog. </p>
     </div>
   );
 }
