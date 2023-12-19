0884bb7fef4c401e12e722edd6a5c4a7c7f0a50c
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-19 15:30:01 -0500

    inital

---
+++ my_portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello World!
+      </h1>
+    </>
+  )
+}
+
+export default App
